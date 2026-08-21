a35fde77c4cee21713f7fd1744b5c985fae4a12d
Gabriel Melo <gmmelo.santos@gmail.com>
2026-08-21 00:54:53 -0300

    feat: add infrastructure dependency injection

---
+++ src/TreasuryFlow.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TreasuryFlow.Application.PaymentOrders.Repositories;
+using TreasuryFlow.Infrastructure.Persistence;
+using TreasuryFlow.Infrastructure.Persistence.Repositories;
+
+namespace TreasuryFlow.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        Action<DbContextOptionsBuilder> configureDbContext)
+    {
+        services.AddDbContext<TreasuryFlowDbContext>(
+            configureDbContext);
+
+        services.AddScoped<
+            IPaymentOrderRepository,
+            PaymentOrderRepository>();
+
+        return services;
+    }
+}
